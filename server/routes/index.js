672ee2d2337f6d718e66707a764d07e4d32ede6f
kim228sy <kim228sy@gmail.com>
2023-07-11 09:57:55 +0900

    mongoDB 연결 테스트

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
-const goodsRouter = require('./goods');
 const spellsRouter = require('./spells');
 const transcribeRouter = require('./transcribe');
 
 const router = express.Router();
 
-router.use('/goods', goodsRouter);
 router.use('/spells', spellsRouter);
 router.use('/transcribe', transcribeRouter);
 
